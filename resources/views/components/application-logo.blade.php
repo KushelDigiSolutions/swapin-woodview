c6e05e74803bc3e8551bf7206862d0f65f4ace43
swapinvidya <swapin@laravelone.in>
2024-02-27 01:02:06 +0530

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-white inline-block mt-2">
+<div class="flex mt-2 text-center" style="width: 100%">
     <img src="{{ asset('logo/logo.png') }}" alt="" style="max-height: 55px">
 </div>
