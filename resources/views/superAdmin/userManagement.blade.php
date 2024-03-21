738ef233754e359543c86dae7007e4b33cf75695
asitmandal <asitmandal492@gmail.com>
2024-03-22 01:53:21 +0530

    this

--- resources/views/superAdmin/userManagement.blade.php
+++ resources/views/superAdmin/userManagement.blade.php
@@ -1,13 +1,6 @@
-    <style>
-          @media only screen and (max-width:585px){
-              .sd{
-                margin:8px 0px;
-              }
-              .trd{
-                margin:8px 0px;
-              }
-          }
-        </style>
+    <!-- <style>
+         
+        </style> -->
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-5 mt-2">
