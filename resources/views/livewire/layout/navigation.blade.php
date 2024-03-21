90d2337c4069afba5f19ded0be6db9df993abdbf
asitmandal <asitmandal492@gmail.com>
2024-03-22 01:34:56 +0530

    this

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -29,7 +29,7 @@ new class extends Component {
                         <x-application-logo/>
                     </a>
                 </div>
-                <button data-drawer-target="sidebar-multi-level-sidebar"
+                <!-- <button data-drawer-target="sidebar-multi-level-sidebar"
                         data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar"
                         type="button"
                         class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -40,7 +40,7 @@ new class extends Component {
                                 d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
                             </path>
                         </svg>
-                    </button>
+                    </button> -->
                 {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     
                 </div> --}}
