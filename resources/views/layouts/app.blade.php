b90183caf54ed8f35d7afeb52b019c2676f0e820
asitmandal <asitmandal492@gmail.com>
2024-03-22 21:09:32 +0530

    this

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,7 +123,10 @@
     <livewire:layout.navigation />
     <div class="sidebar">
         <div>
-         <button id="such">clickjjj</button>
+            <svg id="such" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
         <aside id="sidebar-multi-level-sidebar"
             class="fixed top-15 left-0 z-40 soro w-64 h-[95vh] transition-transform sm:translate-x-0"
             aria-label="Sidebar">
