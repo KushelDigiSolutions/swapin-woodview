8bf691518096dbc2c2b6342a16cb7549f338c116
kushel digi solutions <89138325+KushelDigiSolutions@users.noreply.github.com>
2024-06-05 01:54:36 +0530

    Update survayInvitation.blade.php

--- resources/views/emails/survayInvitation.blade.php
+++ resources/views/emails/survayInvitation.blade.php
@@ -64,7 +64,7 @@
 
         <!-- Footer -->
         <div class="footer">
-            <p>Contact us at <a href="mailto:woodviewhr@woodview.ca">woodviewhr@woodview.ca</a></p>
+            <p>Contact us at <a href="mailto:info@codepxl.com">info@codepxl.com</a></p>
         </div>
     </div>
 </body>
